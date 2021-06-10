fc2dd4f3fe906c302e4b5e207a1cff874e52d5b6
Michael Ellerman <mpe@ellerman.id.au>
2021-06-10 14:57:10 +1000

    Simplify Ubuntu package selection
    Now that we don't support old releases we can just use
    crossbuild-essential-powerpc & gcc-multilib-powerpc-linux-gnu.

--- build/ubuntu/packages.sh
+++ build/ubuntu/packages.sh
@@ -4,33 +4,12 @@ PACKAGES=""
 
 machine=$(uname -m)
 
-PACKAGES+=" gcc-powerpc-linux-gnu g++-powerpc-linux-gnu"
-
 . /etc/os-release
 
-# We could use crossbuild-essential-xxx but that breaks on 14.04
-if [[ "$VERSION_ID" == "14.04" ]]; then
-    PACKAGES+=" gcc-4.8-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "16.04" ]]; then
-    PACKAGES+=" gcc-5-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "18.04" ]]; then
-    PACKAGES+=" gcc-7-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "18.10" ]]; then
-    PACKAGES+=" gcc-8-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "19.04" ]]; then
-    PACKAGES+=" gcc-8-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "20.04" ]]; then
-    PACKAGES+=" gcc-9-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "20.10" ]]; then
-    PACKAGES+=" gcc-10-multilib-powerpc-linux-gnu"
-elif [[ "$VERSION_ID" == "21.04" ]]; then
-    PACKAGES+=" gcc-10-multilib-powerpc-linux-gnu"
-
+if [[ "$VERSION_ID" == "21.04" ]]; then
     if [[ "$machine" != "x86_64" ]]; then
         PACKAGES+=" crossbuild-essential-amd64"
     fi
-else
-    PACKAGES+=" crossbuild-essential-powerpc"
 fi
 
 major="${VERSION_ID%%.*}"
