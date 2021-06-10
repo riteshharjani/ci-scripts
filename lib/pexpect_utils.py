36756ebeca3fe7aa2f5ff98b8ee20c0580b4f7d3
Michael Ellerman <mpe@ellerman.id.au>
2021-06-10 14:57:10 +1000

    qemu: Fix ping test for different ping binaries

--- lib/pexpect_utils.py
+++ lib/pexpect_utils.py
@@ -124,7 +124,8 @@ def standard_boot(p, login=False, user='root', password=None, timeout=-1, prompt
 def ping_test(p, ip='10.0.2.2', check=True):
     p.send(f'ping -W 10 -c 3 {ip}')
     if check:
-        p.expect('3 packets transmitted, 3 packets received')
+        # busybox ping prints "packets received", iputils-ping does not
+        p.expect('3 packets transmitted, 3( packets)? received')
     p.expect_prompt()
 
 
