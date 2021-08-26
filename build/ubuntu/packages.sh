d44fff2adc1f37f6f45a5f08a5e5c9f7194d3bb5
Michael Ellerman <mpe@ellerman.id.au>
2021-08-26 23:45:32 +1000

    Only install clang on Ubuntu 21.04
    It doesn't work on older versions anyway.

--- build/ubuntu/packages.sh
+++ build/ubuntu/packages.sh
@@ -10,10 +10,7 @@ if [[ "$VERSION_ID" == "21.04" ]]; then
     if [[ "$machine" != "x86_64" ]]; then
         PACKAGES+=" crossbuild-essential-amd64"
     fi
-fi
 
-major="${VERSION_ID%%.*}"
-if [[ $major -ge 18 ]]; then
     PACKAGES+=" clang llvm"
 fi
 
