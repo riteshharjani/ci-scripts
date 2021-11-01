riteshharjani/ci-scripts

commit 7ddd70d5f0aa8ed89fde65861cc673200e6f2ed3

Add Ubuntu 21.10 and use it in preference to 21.04

build/ubuntu/packages.sh
@@ -6,7 +6,7 @@ machine=$(uname -m)
 
 . /etc/os-release
 
-if [[ "$VERSION_ID" == "21.04" ]]; then
+if [[ "$VERSION_ID" == "21.10" ]]; then
     if [[ "$machine" != "x86_64" ]]; then
         PACKAGES+=" crossbuild-essential-amd64"
     fi

build/Makefile
@@ -58,11 +58,11 @@ DOCS += docs@${1}@${2}
 endef
 
 FEDORA_DISTROS := fedora@34 fedora@33 fedora@32 fedora@31
-UBUNTU_DISTROS := ubuntu@21.04 ubuntu@20.10 ubuntu@20.04 ubuntu@18.04 ubuntu@16.04
+UBUNTU_DISTROS := ubuntu@21.10 ubuntu@21.04 ubuntu@20.10 ubuntu@20.04 ubuntu@18.04 ubuntu@16.04
 KORG_DISTROS := korg@11.1.0 korg@10.3.0 korg@9.3.0 korg@8.1.0 korg@5.5.0
 ALL_DISTROS := ${UBUNTU_DISTROS} ${KORG_DISTROS} ${FEDORA_DISTROS}
-DOCS_DISTRO := docs@21.04
-X86_DISTRO := ubuntu@21.04
+DOCS_DISTRO := docs@21.10
+X86_DISTRO := ubuntu@21.10
 SUBARCHES := ppc64le ppc64
 
 $(eval $(call MAIN_TEMPLATE,ppc64le,${DOCS_DISTRO}))

commit 5e87834a2e8eea98c278d9f728864f6b343296e0

Install clang on 21.04 or later
Don't drop clang from 21.04 containers, that breaks existing scripts
that use the 21.04 container for clang builds.

Fixes: 7ddd70d5 ("Add Ubuntu 21.10 and use it in preference to 21.04")

build/ubuntu/packages.sh
@@ -6,11 +6,9 @@ machine=$(uname -m)
 
 . /etc/os-release
 
-if [[ "$VERSION_ID" == "21.10" ]]; then
-    if [[ "$machine" != "x86_64" ]]; then
-        PACKAGES+=" crossbuild-essential-amd64"
-    fi
+major="${VERSION_ID%%.*}"
 
+if [[ "$major" -ge 21 ]]; then
     PACKAGES+=" clang llvm"
 fi
 
@@ -22,6 +20,11 @@ if [[ "$machine" == "ppc64le" ]]; then
     PACKAGES+=" libhugetlbfs-dev"
     PACKAGES+=" libmnl-dev"
     PACKAGES+=" libmount-dev"
+
+    # For the x86_64 container
+    if [[ "$VERSION_ID" == "21.10" ]]; then
+        PACKAGES+=" crossbuild-essential-amd64"
+    fi
 else
     PACKAGES+=" gcc-powerpc64le-linux-gnu g++-powerpc64le-linux-gnu"
 fi