riteshharjani/ci-scripts

commit 158aba83877a24465d98ec84bd53ae946344e88e

qemu: Simplyify pexpect timeout handling
Give each pexpect call 60 seconds by default.

Calculate the other timeouts based on that, by default allow 5 minutes
to boot, that is the value we were previously using for cloud image
boots, for cpio boots it gives them longer than before.

Set the global timeout at 10 x the pexpect timeout.

lib/qemu.py
@@ -140,8 +140,6 @@ def qemu_main(qemu_machine, cpuinfo_platform, cpu, net, args):
 
     cloud_image = os.environ.get('CLOUD_IMAGE', False)
     if cloud_image:
-        boot_timeout = 300
-
         # Create snapshot image
         rdpath = get_root_disk_path()
         src = f'{rdpath}/{cloud_image}'
@@ -170,7 +168,6 @@ def qemu_main(qemu_machine, cpuinfo_platform, cpu, net, args):
         drive += f'-drive file={dst},format=qcow2,if={interface},id=drive0 ' \
                  f'-drive file={rdpath}/cloud-init-user-data.img,format=raw,if={interface},readonly=on,id=drive1'
     else:
-        boot_timeout = 120
         drive = None
 
     host_mount = os.environ.get('QEMU_HOST_MOUNT', '')
@@ -180,14 +177,13 @@ def qemu_main(qemu_machine, cpuinfo_platform, cpu, net, args):
 
     cmdline += get_env_var('LINUX_CMDLINE', '')
 
-    p = PexpectHelper()
+    # Default timeout for a single pexpect call
+    pexpect_timeout = 60
 
+    gdb = None
     if '--gdb' in args:
         gdb = '-s -S'
-        p.timeout = None
-        boot_timeout = None
-    else:
-        gdb = None
+        pexpect_timeout = 0
 
     cmd = qemu_command(machine=qemu_machine, cpu=cpu, mem='4G', smp=smp, vmlinux=vmlinux,
                        drive=drive, host_mount=host_mount, cmdline=cmdline, accel=accel,
@@ -198,16 +194,18 @@ def qemu_main(qemu_machine, cpuinfo_platform, cpu, net, args):
         rc = subprocess.run(cmd, shell=True).returncode
         return rc == 0
 
-    if not gdb:
-        setup_timeout(600)
+    setup_timeout(10 * pexpect_timeout)
+    boot_timeout = pexpect_timeout * 5
 
     logpath = get_env_var('QEMU_CONSOLE_LOG', 'console.log')
-    p.spawn(cmd, logfile=open(logpath, 'w'))
+
+    p = PexpectHelper()
+    p.spawn(cmd, logfile=open(logpath, 'w'), timeout=pexpect_timeout)
 
     if cloud_image:
         standard_boot(p, prompt=prompt, login=True, password='linuxppc', timeout=boot_timeout)
     else:
-        standard_boot(p)
+        standard_boot(p, timeout=boot_timeout)
 
     p.send("echo -n 'booted-revision: '; uname -r")
     p.expect(f'booted-revision: {expected_release}')

lib/pexpect_utils.py
@@ -21,11 +21,10 @@ class PexpectHelper:
         self.prompt = None
         self.prompt_stack = []
         self.bug_patterns = self.default_bug_patterns
-        self.timeout = 60
 
     def spawn(self, *args, **kwargs):
         logging.info("Spawning '%s'" % args)
-        self.child = pexpect.spawn(*args, timeout=self.timeout, encoding='utf-8', echo=False, **kwargs)
+        self.child = pexpect.spawn(*args, encoding='utf-8', echo=False, **kwargs)
         if '--quiet' not in sys.argv:
             self.log_to(sys.stdout)
 

commit b4af7d52186a7e1ce898d37fd21a5589c65d26b7

qemu: Fix timeout handling for gdb
When using gdb we don't want any timeouts. To communicate that to
pexpect we need to pass a timeout of None, not zero.

Fixes: 158aba83 ("qemu: Simplyify pexpect timeout handling")

lib/qemu.py
@@ -205,7 +205,10 @@ def qemu_main(qemu_machine, cpuinfo_platform, cpu, net, args):
         return rc == 0
 
     setup_timeout(10 * pexpect_timeout)
-    boot_timeout = pexpect_timeout * 5
+    if pexpect_timeout:
+        boot_timeout = pexpect_timeout * 5
+    else:
+        boot_timeout = pexpect_timeout = None
 
     logpath = get_env_var('QEMU_CONSOLE_LOG', 'console.log')
 